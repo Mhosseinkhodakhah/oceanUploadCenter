c8b773053cf4d9638200cd981e7972c7b471401a
Mhosseinkhodakhah <hosseinkhodakhah6@gmail.com>
2024-12-21 00:55:52 +0330

    asdf

--- dist/src/router.js
+++ dist/src/router.js
@@ -7,6 +7,6 @@ const express_1 = require("express");
 const controller_1 = __importDefault(require("./controller"));
 const router = (0, express_1.Router)();
 const uploadController = new controller_1.default();
-router.post('/upload-multi-photo', uploadController.uploadMultiPhoto);
+router.post('/upload-multi-photo/:contentId', uploadController.uploadMultiPhoto);
 router.post('/upload-profile/:userId', uploadController.uploadProfile);
 exports.default = router;
